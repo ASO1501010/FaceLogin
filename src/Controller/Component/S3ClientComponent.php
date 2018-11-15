fde8f7d0a9443ee424c0e65ac6f479d2eacd4c7f
abccsystem <38305276+abccsystem@users.noreply.github.com>
2018-11-15 12:52:14 +0900

    feat: ログイン処理の追加

--- src/Controller/Component/S3ClientComponent.php
+++ src/Controller/Component/S3ClientComponent.php
@@ -105,6 +105,8 @@ class S3ClientComponent extends Component
                 'Key'    => $s3_file_path,
             ]);
 
+            $this->log($result);
+
             return $result;
         } catch (S3Exception $e) {
             echo $e->getMessage();
